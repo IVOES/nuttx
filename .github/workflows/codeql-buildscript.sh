8eb80e2392adf3de1e857418b3672c52c95c422b
Brian <89487381+b4yuan@users.noreply.github.com>
2024-02-06 16:20:50 -0500

    Update codeql-buildscript.sh

--- .github/workflows/codeql-buildscript.sh
+++ .github/workflows/codeql-buildscript.sh
@@ -1,24 +1,5 @@
 #!/usr/bin/env bash
 
-sudo apt-get install kconfig-frontends
+sudo apt-get install kconfig-frontends gcc-arm-none-eabi
 
-git clone https://github.com/apache/nuttx-apps.git ../apps
-
-TOPDIR=.
-DUMPCFGS=""
-configlist=`find ${TOPDIR}/boards/arm -name defconfig`
-for defconfig in ${configlist}; do
-  config=`dirname ${defconfig} | sed -e "s,${TOPDIR}/boards/,,g"`
-  boardname=`echo ${config} | cut -d'/' -f3`
-  configname=`echo ${config} | cut -d'/' -f5`
-  DUMPCFGS="${DUMPCFGS} ${boardname}:${configname}"
-done
-
-for cfg in $(echo "$DUMPCFGS" | sed 's, ,\n,g' | sort -t ':' -k2 -u); do
-  echo "Compiling $cfg"
-  ./tools/configure.sh $cfg
-  make CROSSDEV=arm-none-eabi- -j$(nproc)
-  make distclean
-done
-
-rm -rf ../apps
+make
